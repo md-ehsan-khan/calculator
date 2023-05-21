46720ed39ea6595b54c5cdc907afbf5269bf61d6
ehsankhan-blitz <ehsan.khan@blitzscale.co>
2023-05-21 18:25:26 +0530

    fixed minus constant

--- src/shared/constants/buttonConstants.js
+++ src/shared/constants/buttonConstants.js
@@ -1,5 +1,5 @@
 
 export const btnsLevel1 = ['7', '8', '9', 'x']; 
-export const btnsLevel2 = ['4', '5', '6', '_']; 
+export const btnsLevel2 = ['4', '5', '6', '-']; 
 export const btnsLevel3 = ['1','2','3','+']; 
 export const btnsLevel4 = [ 'C','0','=','/'];
